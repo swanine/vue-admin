df8fa1ff16f0c006dd9cc6c74c92baa0f0dd2ab9
swanine <haohao1500@foxmail.com>
2021-08-21 19:45:39 +0800

    request封装

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-import axios from 'axios'
+import request from './utils/request'
 
 console.log('环境变量=>', import.meta.env)
+
 const app = createApp(App)
-app.use(router).use(ElementPlus).use(axios)
+app.use(router).use(ElementPlus)
+app.config.globalProperties.$request = request
 app.mount('#app')
